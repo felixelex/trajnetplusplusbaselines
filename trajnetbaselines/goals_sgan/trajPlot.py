4af31f86c099478e17060a8a8c54fe07bbe9ea64
felixelex <60747489+felixelex@users.noreply.github.com>
2021-05-31 09:41:00 +0200

    plotting trajectories and goals

--- trajnetbaselines/goals_sgan/trajPlot.py
+++ trajnetbaselines/goals_sgan/trajPlot.py
@@ -28,8 +28,23 @@ scene = trajnetplusplustools.Reader.paths_to_xy(paths)
 scene = np.array(scene)
 # scene [length, actors, xy]
 
+
+## Predict goal
+batch_split = torch.Tensor([0, scene.shape[1]])
+goals = goal_Model(torch.Tensor(scene), batch_split)
+goals = goals.detach().numpy()
+
+## Plot
 for i in range(scene.shape[1]):
-    plt.plot(scene[obs_length, i, 0], scene[obs_length, i, 1])
+    if i==0:
+        plt.plot(scene[:obs_length, i, 0], scene[:obs_length, i, 1], 'r-')
+        plt.plot(scene[obs_length-1:, i, 0], scene[obs_length-1:, i, 1], 'r--')
+    else:
+        plt.plot(scene[:obs_length, i, 0], scene[:obs_length, i, 1], 'k-')
+        plt.plot(scene[obs_length-1:, i, 0], scene[obs_length-1:, i, 1], 'k--')
+        
+for i in range(goals.shape[1]):
+    plt.scatter(goals[0, i, 0], goals[0, i, 1], c='r', marker='*')        
 
 plt.show()
 
