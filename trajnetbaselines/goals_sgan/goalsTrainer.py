1b02ad23933b187f4eb394d0b11172e527ab10db
felixelex <60747489+felixelex@users.noreply.github.com>
2021-05-31 15:04:51 +0200

    added input assert for goalModel

--- trajnetbaselines/goals_sgan/goalsTrainer.py
+++ trajnetbaselines/goals_sgan/goalsTrainer.py
@@ -252,9 +252,9 @@ class GoalsTrainer(object):
         seq_length = batch_scene.shape[0]
         batch_scene = interpolate_batch_scene(batch_scene, seq_length)
         
+        assert not batch_scene.isnan().any(), 'NaNs in batch_scene'
         goal_pred = self.model(batch_scene, obs_len=self.obs_length)
         assert not goal_pred.isnan().any(), 'NaNs in goal_pred: \n{}'.format(goal_pred)
-        print("It worked!")
         
         # goal_pred [num_tracks, k, out_dim=2]
         loss = self.criterion(goal_pred, goal_gt)
